1044c00ad03df5114729e9b504828930574e027f
Brayam <bhuamanpc@gmail.com>
2024-05-19 22:57:23 -0500

    image network & local

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,23 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               SizedBox(
-                height: 250,
-                width: 400,
-                child: Image.asset("assets/profile.jpeg"),
-              )
+                height: 300.0,
+                width: 600.0,
+                
+                child: Image.asset("assets/profile.jpeg", fit: BoxFit.fitHeight,),
+              ),
+
+              SizedBox(
+                height: 300.0,
+                width: 800.0,
+                
+                child: Image.network("https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2p2Y283ZXZxamVpdGNjenQwNDJxa283c3p5ZHU5MmtmaGN0Z2o3YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2IudUHdI075HL02Pkk/giphy.gif", fit: BoxFit.fitHeight,),
+              ),
+
+
             ],
           ),
         ),
